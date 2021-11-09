49380740f68f2afc26486890e021d0c72e2f44c9
DayoungChoii <young12430@gmail.com>
2021-11-10 00:45:39 +0900

    [spring기초] 스프링프로젝트생성
     - springboot + gradle

---
+++ src/main/java/dayoung/example/dayoungspring/DayoungSpringApplication.java
@@ -0,0 +1,13 @@
+package dayoung.example.dayoungspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayoungSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayoungSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dayoung/example/dayoungspring/DayoungSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dayoung.example.dayoungspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayoungSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
