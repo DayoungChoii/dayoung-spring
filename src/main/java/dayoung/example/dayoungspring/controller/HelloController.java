59af9c23894c340dd6e371169679855bc51ea1a5
DayoungChoii <young12430@gmail.com>
2021-11-10 22:52:07 +0900

    [spring 기초] View 환경설정
     - static resources index.html 추가
     - template에 index.html 추가

---
+++ src/main/java/dayoung/example/dayoungspring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package dayoung.example.dayoungspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello-test-test!!");
+        return "hello";
+    }
+}
