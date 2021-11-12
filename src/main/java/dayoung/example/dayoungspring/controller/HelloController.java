477ac1db0a579765344aae53f938d3622b135a45
DayoungChoii <young12430@gmail.com>
2021-11-13 02:18:50 +0900

    [spring 기초] 스프링 웹개발 기초
     - MVC와 템플릿 엔진

--- src/main/java/dayoung/example/dayoungspring/controller/HelloController.java
+++ src/main/java/dayoung/example/dayoungspring/controller/HelloController.java
@@ -3,6 +3,7 @@ package dayoung.example.dayoungspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,12 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello-test-test!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+
+        return "hello_template";
     }
 }
