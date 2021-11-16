05ebf1862bbdd897e4875d42c52cf4bd0aa1e965
DayoungChoii <young12430@gmail.com>
2021-11-16 22:58:13 +0900

    [spring 기초] 스프링 웹개발 기초
     - 회원관리 예제 - 백엔드 개발, 회원 도메인과 리포지토리 만들기

---
+++ src/main/java/dayoung/example/dayoungspring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package dayoung.example.dayoungspring.repository;
+
+import dayoung.example.dayoungspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
