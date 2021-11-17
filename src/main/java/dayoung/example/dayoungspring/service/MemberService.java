501ddcdf305d2f9b13401a7e914640794ecc9d3c
DayoungChoii <young12430@gmail.com>
2021-11-18 00:25:03 +0900

    [spring 기초] 스프링 웹개발 기초
    - 회원관리 예제 - 백엔드 개발, 회원 서비스 개발
    - repository는 curd위주의 이름, service는 비지니스 로직을 포괄하는 이름으로 짓는다

---
+++ src/main/java/dayoung/example/dayoungspring/service/MemberService.java
@@ -0,0 +1,46 @@
+package dayoung.example.dayoungspring.service;
+
+import dayoung.example.dayoungspring.domain.Member;
+import dayoung.example.dayoungspring.repository.MemberRepository;
+import dayoung.example.dayoungspring.repository.MemoryMemberRepository;
+import java.util.List;
+
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        /*Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(member1 -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });*/
+        
+        //권장하는 optional의 모양새
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
