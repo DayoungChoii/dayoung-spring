ab48cd7bc371c8b7c1565dc37c30412262a3e871
DayoungChoii <young12430@gmail.com>
2021-11-24 22:40:51 +0900

    [spring 기초] 스프링 웹개발 기초
     - 회원 관리 예제 - 웹 mvc개발
     - 회원 웹 기능 - 홈 화면 추가
     - 최원 웹 기능 - 등록

---
+++ src/main/java/dayoung/example/dayoungspring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package dayoung.example.dayoungspring.controller;
+
+public class MemberForm {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
+}
