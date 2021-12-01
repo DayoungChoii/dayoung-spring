7553d1a83884bdaecd552b0a1417f9890f25a01f
DayoungChoii <young12430@gmail.com>
2021-12-01 22:06:56 +0900

    [spring 기초] 스프링 웹개발 기초
     - AOP
     -  1000개의 메소드에 메소드 수행시간을 측정해야하는 상황

--- src/main/java/dayoung/example/dayoungspring/service/MemberService.java
+++ src/main/java/dayoung/example/dayoungspring/service/MemberService.java
@@ -23,6 +23,21 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member){
+
+        long start = System.currentTimeMillis();
+
+        try{
+            validateDuplicateMember(member); //중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("jon = " + timeMs + "ms");
+
+        }
+
+
         //같은 이름이 있는 중복 회원X
         /*Optional<Member> result = memberRepository.findByName(member.getName());
         result.ifPresent(member1 -> {
@@ -30,9 +45,7 @@ public class MemberService {
         });*/
         
         //권장하는 optional의 모양새
-        validateDuplicateMember(member); //중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -40,13 +53,24 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+
+
     }
 
     /**
      * 전체 회원조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try{
+            return memberRepository.findAll();
+        } finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId){
